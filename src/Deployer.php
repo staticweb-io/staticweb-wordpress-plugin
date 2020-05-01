49874d00b49e8e78d2cf9304771a96100aa5d634
John Shaffer <jshaffer2112@protonmail.com>
2020-05-01 18:46:06 -0500

    Detect when redirects are needed, upload them, and update WP2Static's DeployCache.

--- src/Deployer.php
+++ src/Deployer.php
@@ -2,14 +2,25 @@
 
 namespace StaticWeb;
 
-use Aws\S3\S3Client;
-use WP2Static\CoreOptions;
-use WP2Static\ProcessedSite;
-use WP2StaticS3\Controller;
+use WP2Static\WsLog;
 
 define("MD5_EMPTY_STRING", "d41d8cd98f00b204e9800998ecf8427e");
 
 class Deployer {
+
+    public static function add_to_deploy_cache( string $local_path, string $hash ) : void {
+        global $wpdb;
+
+        $deploy_cache_table = $wpdb->prefix . 'wp2static_deploy_cache';
+        $post_processed_dir = \WP2Static\ProcessedSite::getPath();
+        $deployed_file = $post_processed_dir . $local_path;
+        $path_hash = md5( $deployed_file );
+
+        $sql = "INSERT INTO {$deploy_cache_table} (path_hash,path,file_hash)" .
+            ' VALUES (%s,%s,%s) ON DUPLICATE KEY UPDATE file_hash = %s';
+        $sql = $wpdb->prepare( $sql, $path_hash, $local_path, $hash, $hash );
+        $wpdb->query( $sql );
+    }
 
     public static function deploy( string $processed_site_path ) : void {
         global $wpdb;
@@ -47,10 +58,12 @@ class Deployer {
             unset( $client_options['profile'] );
         }
 
+        $site_path = rtrim( \WP2Static\SiteInfo::getURL( 'site' ), '/' );
+        $client = new HTTPClient();
+
         // instantiate S3 client
         $s3 = new \Aws\S3\S3Client( $client_options );
         $bucket = \WP2StaticS3\Controller::getValue( 's3Bucket' );
-        $post_processed_dir = \WP2Static\ProcessedSite::getPath();
 
         $table_name = $wpdb->prefix . 'staticweb_permalinks';
         $query = "SELECT * FROM $table_name WHERE deployed IS NULL OR deployed <= updated";
@@ -62,22 +75,52 @@ class Deployer {
             $path = $row->relative_permalink;
             $canonical_path = wp_make_link_relative(get_permalink($post_id));
 
+            $remote_path = \WP2StaticS3\Controller::getValue( 's3RemotePath' );
+            if ($remote_path) $remote_path = $remote_path . '/';
+
             if ( is_null( $post ) ) {
                 self::delete_row( $row->id );
             } else if ( 'publish' === $post->post_status ) {
                 if ($path !== $canonical_path) {
-                    if ( mb_substr( $path, -1 ) === '/' ) {
-                        $path = $path . "index.html";
+                    $url = new \WP2Static\URL( $site_path . $path );
+                    $response = $client->getURL( $url );
+                    $status = $response['code'];
+
+                    if (200 <= $status && $status < 300) {
+                        // If the server returns a valid page response,
+                        // we don't write a redirect.
+                        self::update_deployed( $row->id, $row->updated );
+                    } else if (300 <= $status && $status < 500) {
+                        // If the server returns a status from 300-499,
+                        // create a redirect.
+                        if ( mb_substr( $path, -1 ) === '/' ) {
+                            $key = $path . "index.html";
+                        } else {
+                            $key = $path;
+                        }
+
+                        $s3_key = $remote_path . ltrim($key, '/');
+                        $s3_response = self::put_redirect( $s3, $bucket, $s3_key, $canonical_path );
+                        if (200 === $s3_response->get('@metadata')['statusCode']) {
+                            self::add_to_deploy_cache( $key, MD5_EMPTY_STRING );
+                            self::update_deployed( $row->id, $row->updated );
+                        } else {
+                            WsLog::l('StaticWeb: AWS response status ' .
+                                     $s3_response->get('@metadata')['statusCode']);
+                        }
+                    } else {
+                        // This shouldn't happen.
+                        WsLog::l('StaticWeb: ' . $status . ' for URL ' . $site_path . $path);
                     }
-                    $file_path = $post_processed_dir . $path;
                 } else {
+                    // If this is the post's current URL, skip it.
                     self::update_deployed( $row->id, $row->updated );
                 }
             } else {
+                // If the post has any status other than publish, skip it.
                 self::update_deployed( $row->id, $row->updated );
             }
         }
-
     }
 
     public static function delete_row ( int $id ) : void {
@@ -90,7 +133,7 @@ class Deployer {
     }
 
     public static function put_redirect( \Aws\S3\S3Client $s3, string $bucket,
-                                         string $key, string $redirect_to ) : array {
+                                         string $key, string $redirect_to ) : object {
         $result = $s3->putObject(
             [
                 'Bucket' => $bucket,
---
+++ src/HTTPClient.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace StaticWeb;
+
+// Modified from https://github.com/WP2Static/wp2static/blob/fe4877fe235c3cc73fbda923adfbd50c8d82c9e8/src/Crawler.php
+
+class HTTPClient {
+
+    /**
+     * @var resource | bool
+     */
+    private $ch;
+    /**
+     * @var Request
+     */
+    private $request;
+    /**
+     * @var mixed[]
+     */
+    private $curl_options;
+
+    /**
+     * Crawler constructor
+     */
+    public function __construct() {
+        $this->ch = curl_init();
+        $this->request = new \WP2Static\Request();
+
+        $port_override = apply_filters(
+            'wp2static_curl_port',
+            null
+        );
+
+        // TODO: apply this filter when option is saved
+        if ( $port_override ) {
+            curl_setopt( $this->ch, CURLOPT_PORT, $port_override );
+        }
+
+        curl_setopt(
+            $this->ch,
+            CURLOPT_USERAGENT,
+            apply_filters( 'wp2static_curl_user_agent', 'WP2Static.com' )
+        );
+
+        $auth_user = \WP2Static\CoreOptions::getValue( 'basicAuthUser' );
+        $auth_password = \WP2Static\CoreOptions::getValue( 'basicAuthPassword' );
+
+        if ( $auth_user || $auth_password ) {
+            curl_setopt(
+                $this->ch,
+                CURLOPT_USERPWD,
+                $auth_user . ':' . $auth_password
+            );
+        }
+    }
+
+    public function getURL( \WP2Static\URL $url ) : array {
+        return $this->request->getURL( $url->get(), $this->ch );
+    }
+}
