4ea3cdb497cbd273a4a6ca6200d68a987f0721e2
John Shaffer <jshaffer2112@protonmail.com>
2020-05-05 12:20:03 -0500

    Version 1.1.0-alpha.

--- staticweb-wordpress-plugin.php
+++ staticweb-wordpress-plugin.php
@@ -4,7 +4,7 @@
  * Plugin Name:       StaticWeb
  * Plugin URI:        https://staticweb.io
  * Description:       StaticWeb Plugin
- * Version:           1.0.1
+ * Version:           1.1.0-alpha
  * Author:            StaticWeb.io
  * Author URI:        https://staticweb.io
  */
@@ -14,7 +14,7 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 define( 'STATICWEB_PATH', plugin_dir_path( __FILE__ ) );
-define( 'STATICWEB_VERSION', '1.0.1' );
+define( 'STATICWEB_VERSION', '1.1.0-alpha' );
 
 require STATICWEB_PATH . 'vendor/autoload.php';
 
